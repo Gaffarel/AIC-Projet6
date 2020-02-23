8ee072b9f0690c470c5343094af4d263b82f5439
Gaffarel <49238255+Gaffarel@users.noreply.github.com>
2020-02-23 14:33:40 +0100

    Mise à jour programme WPB.py Version V0.0b

--- WPB.py
+++ WPB.py
@@ -2,7 +2,7 @@
 
 #####################################################################
 ##                                                                 ##
-##     Script de sauvegarde et restauration wordpresss  V0.0a      ##
+##     Script de sauvegarde et restauration wordpresss  V0.0b      ##
 ##                                                                 ##
 #####################################################################
 
@@ -14,25 +14,55 @@
 #####################################################################
 
 #import os
-import os.path
-
-#help(os.path ) #aide sur la commande
+import os.path #
+import docker #
+import datetime #
+from datetime import date #
 
 #####################################################################
 ##                                                                 ##
-##                    Importation des modules                      ##
+##                         Les Variables                           ##
 ##                                                                 ##
 #####################################################################
 
-#os.makedirs('/home/backup', exist_ok=True) 
+############################ rétention ##############################
 
-# Vérifier si le chemin existe ou non
-path = '/home/save'
-path2 = 'c:\\save'
+nbjour=10
 
-if os.path.exists(path2) :
-    print("Chemin " , path2, " existe")
+############################## Temps ################################
+
+date_aujourdhui = date.today() # date d'aujourd'hui
+date_aujourdhui_retention = date_aujourdhui-datetime.timedelta(days=nbjour) # date d'aujourd'hui - la date de retention désiré
+BACKUPDATE = date_aujourdhui.strftime("%d-%m-%Y") # formatage de la date Jour/Mois/Année
+BACKUPDATE_OLD = date_aujourdhui_retention.strftime("%d-%m-%Y") # formatage de la date Jour/Mois/Année
+
+############################ répertoire #############################
+
+repertoire_de_sauvegarde = '/home/save' # répertoire de sauvegarde linux
+
+#####################################################################
+##                                                                 ##
+##                    Programme de Backup                          ##
+##                                                                 ##
+#####################################################################
+
+# Vérifier si le répertoire "/home/save" existe ou non
+
+if os.path.exists(repertoire_de_sauvegarde) :
+     print("Chemin " , repertoire_de_sauvegarde, " existe")
 else:
-    os.makedirs('c:\\save', exist_ok=True) 
-    print("Chemin " , path2, " n'existe pas")
+     os.makedirs(repertoire_de_sauvegarde, exist_ok=True)
+     print("Chemin " , repertoire_de_sauvegarde, " n'existe pas")
 
+# Récupération du nom et de l'ID du conteneur
+
+# client = docker.from_env()
+# for container in client.containers.list():
+#  print (container.image,container.id)
+
+
+
+
+
+print(BACKUPDATE)
+print(BACKUPDATE_OLD)
