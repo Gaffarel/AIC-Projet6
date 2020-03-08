1b6f2ae8d8f2f821469bafe9afd40f892186c5fc
Gaffarel <49238255+Gaffarel@users.noreply.github.com>
2020-03-08 15:01:14 +0100

    Mise à jour programme create.py Version V0.1

---
+++ create.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+#####################################################################
+##                                                                 ##
+##    Script de création d'un serveur wordpress et MariaDB  V0.3   ##
+##                                                                 ##
+#####################################################################
+
+
+#####################################################################
+##                                                                 ##
+##                    Importation des modules                      ##
+##                                                                 ##
+#####################################################################
+
+import os # Diverses interfaces pour le système d'exploitation
+import os.path # manipulation courante des chemins
+import platform # modules pour vérifier la platform (Linux/Windows/Mac)
+import datetime # Types de base pour la date et l'heure
+import configparser # Configuration file parser
+import docker # Docker
+from datetime import date # 
+import tarfile # 
+from azure.storage.file import FileService # 
+
+#####################################################################
+##                                                                 ##
+##                         Les Variables                           ##
+##                                                                 ##
+#####################################################################
+
+################ Import du fichier de configuration #################
+
+config = configparser.ConfigParser()
+config.read('P6_config.ini')
+AZURE_CPT = config.get('config','azure_login')
+AZURE_KEY = config.get('config','azure_key')
+AZURE_REP_BKP = config.get('config','azure_bkp')
+UserBDD = config.get('config','user_bdd')
+MdpBDD = config.get('config','mdp_bdd')
+NBjourDEretention = config.get('retention','nbjour')
+repertoire_de_sauvegarde = config.get('repertoire','backup_repertoire')
+
+############################## Temps ################################
