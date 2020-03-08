feaa34c907705c4639e2eb313f7f175c36430273
Gaffarel <49238255+Gaffarel@users.noreply.github.com>
2020-03-08 17:29:19 +0100

    Mise à jour programme restore.py Version V0.1

--- restore.py
+++ restore.py
@@ -2,7 +2,7 @@
 
 #####################################################################
 ##                                                                 ##
-## Script de restauration d'un serveur wordpress et MariaDB  V0.3  ##
+## Script de restauration d'un serveur wordpress et MariaDB  V0.1  ##
 ##                                                                 ##
 #####################################################################
 
